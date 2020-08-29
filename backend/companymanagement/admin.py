3e579badd744aff0118d4e9bc3b32c9821d8af6a
yxnyxnyxnxyxnyxnyxn <yxn1116@gmail.com>
2020-08-29 15:51:14 -0400

    serializer

--- backend/companymanagement/admin.py
+++ backend/companymanagement/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Company
+
+
 # Register your models here.
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name','description','industry','location','CEO','founded')
+    list_display = ('name', 'description', 'industry', 'location', 'CEO', 'founded')
 
-admin.site.register(Company,CompanyAdmin)
+
+admin.site.register(Company, CompanyAdmin)
--- backend/companymanagement/views.py
+++ backend/companymanagement/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import CompanySerializer
+from .models import Company
+
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
